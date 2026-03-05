c3c140b60d61e0f63e52e3abbe3510a1c535e638
namtu06 <namtuhoanghai06@gmail.com>
2026-03-05 20:45:05 +0200

    initialize week 1, did q1

---
+++ Week1/t1_q1.sql
@@ -0,0 +1,5 @@
+-- Kirjoita SQL-lauseesi alle:
+-- Write your SQL statement below:
+SELECT * 
+FROM bike
+ORDER BY id ASC;
---
+++ Week1/test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employee (
+id INT NOT NULL,
+first_name VARCHAR(20) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+locality VARCHAR(30) NOT NULL,
+salary NUMERIC(8,2),
+phone VARCHAR(15),
+PRIMARY KEY (id));
